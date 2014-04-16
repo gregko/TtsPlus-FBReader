be7ef741e12369aef74f36f0a34524db8a574bf3
Greg Kochaniak <gregko@hyperionics.com>
2014-04-16 18:58:49 -0400

    v. 3.4.7

--- src/com/hyperionics/fbreader/plugin/tts_plus/TtsSentenceExtractor.java
+++ src/com/hyperionics/fbreader/plugin/tts_plus/TtsSentenceExtractor.java
@@ -111,12 +111,14 @@ public class TtsSentenceExtractor {
                     w = w.replace('\u2013', '-'); // dec 8211, "en dash" or long dash, Ivona PL reads as "przecinek"
                     w = w.replace('\u2014', '-'); // dec 8211, 'EM DASH', Ivona PL reads as "przecinek"
                 } else if (loc.getISO3Language().equals("spa")) {
-                    int n = w.indexOf('\u2014');
-                    if (n == 0 && w.length() > 1) {
-                        char c = w.charAt(1);
-                        if (c == '.' || c == '?' || c=='!')
-                            w = w.replace(c, ' '); // add space to avoid "geeon" in Ivona
-                    }
+                    w = w.replace('\u2013', ','); // dec 8211, "en dash" or long dash, Ivona, Samsung voices reads as "geeon"
+                    w = w.replace('\u2014', ','); // dec 8211, 'EM DASH', Ivona, Samsung voices read as "geeon"
+//                    int n = w.indexOf('\u2014');
+//                    if (n == 0 && w.length() > 1) {
+//                        char c = w.charAt(1);
+//                        if (c == '.' || c == '?' || c=='!')
+//                            w = w.replace(c, ' '); // add space to avoid "geeon" in Ivona
+//                    }
                 }
                 w = w.replace('\u00A0', ' '); // dec 160, no-break space
                 w = w.replace("\u200B", " ");  // dec. 8203, 'zero width space' (do not replace with empty, or we may get w empty and crash)
